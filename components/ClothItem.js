bf20512597395104103cc004483571387a577940
ClaraLee1 <97991874+ClaraLee1@users.noreply.github.com>
2022-11-18 20:54:02 -0800

    add img uri

--- components/ClothItem.js
+++ components/ClothItem.js
@@ -5,21 +5,23 @@ import { View, Text, StyleSheet, Image } from "react-native";
 function ClothItem ({cloth}) {
     return (
         <View style={styles.container}>
+         
             <View style={styles.innercontainer}>
-                 <View>
-                    <Image 
-                        style={styles.tinyLogo}
-                        source={{
-                            uri: cloth.uri,
-                        }}
-                    />
-                </View>
+                <Image 
+                    style={styles.tinyLogo}
+                    source={{
+                        uri: cloth.uri !== ""? cloth.uri : 'https://reactnative.dev/img/tiny_logo.png',
+                    }}
+                />
+            </View>
 
-                <View>
-                    <Text style={styles.itemTitle}>
-                        {cloth.title}
-                    </Text>
-                </View>
+            <View style={styles.innercontainer}>
+                <Text style={styles.itemTop}>
+                    Top 1
+                </Text>
+                <Text style={styles.itemTitle}>
+                    {cloth.title}
+                </Text>
             </View>
         </View>
     );
@@ -30,42 +32,26 @@ const styles = StyleSheet.create({
         backgroundColor:'#e5e5e5',
         padding:15,
         borderRadius:15,
-        margin:5,
-        marginHorizontal:10,
+        margin:15,
+        marginHorizontal:25,
+        flexDirection: "row",
     },
-    itemTitle: {
-        fontSize: 18,
+    innercontainer: {
+        flex:1,
+    },
+    itemTop: {
+        color: 'red',
+        fontSize: 20,
         fontWeight:'bold',
     },
-    itemContent: {
-        fontWeight: '300'
+    itemTitle: {
+        fontSize: 16,
+        fontWeight:'bold',
     },
     tinyLogo: {
-        width: 50,
-        height: 50,
+        width: 120,
+        height: 120,
     },
 });
 
 export default ClothItem;
-
-
-// const styles = StyleSheet.create({
-//     container: {
-//       backgroundColor:'#e5e5e5',
-//       padding:15,
-//       borderRadius:15,
-//       margin:5,
-//       marginHorizontal:10,
-//     },
-//     innercontainer: {
-//       alignItems:'center',
-//       flexDirection:'column',
-//     },
-//     itemTitle:{
-//       fontWeight:'bold', 
-//     },
-//     itemContent:{
-//       fontWeight:'300'
-//     }
-//   })
-  
