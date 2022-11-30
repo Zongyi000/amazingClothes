82a8a431717e9034bb5807242533e927884813b9
ClaraLee1 <97991874+ClaraLee1@users.noreply.github.com>
2022-11-30 00:35:42 -0800

    add posts in profile

--- components/MyProfile.js
+++ components/MyProfile.js
@@ -1,14 +1,168 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { styles } from '../styles'
+// import React from 'react';
+// import { View, Text, Button } from 'react-native';
+// import { styles } from '../styles'
 import { auth } from "../Firebase/firebase-setup";
 import {signOut} from "firebase/auth";
+import { collection, onSnapshot } from 'firebase/firestore';
+import { firestore } from '../Firebase/firebase-setup';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default MyProfile = () =>
-  <View style={styles.center}>
-    {/* <Text style={styles.title}>MyProfile</Text> */}
-    <Text>{auth.currentUser.email}</Text>
-    <Text>{auth.currentUser.uid}</Text>
-    <Button title="Logout" onPress={() => signOut(auth)} />
-  </View>
+const Stack = createNativeStackNavigator();
+// export default MyProfile = () =>
+//   <View style={styles.center}>
+//     {/* <Text style={styles.title}>MyProfile</Text> */}
+//     <Text>{auth.currentUser.email}</Text>
+//     <Text>{auth.currentUser.uid}</Text>
+//     <Button title="Logout" onPress={() => signOut(auth)} />
+//   </View>
 
+import React, {useState, useEffect, useContext} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  FlatList
+} from 'react-native';
+
+import ClothItem from '../components/ClothItem';
+
+const MyProfile = ({navigation, route}) => {
+
+  const [clothes, setClothes] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(firestore, "clothes"),
+      (querySnapshot) => {
+        if (querySnapshot.empty) {
+          setClothes([]);
+          return;
+        }
+        setClothes(
+          querySnapshot.docs.map((snapDoc) => {
+            let data = snapDoc.data();
+            data = { ...data, key: snapDoc.id };
+            return data;
+          })
+        );
+      }
+    );
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+      <View style={styles.container}>
+        <Image
+          style={styles.userImg}
+          source={{uri: 'https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg'}}
+        />
+        <Text style={styles.userName}>User Name</Text>
+        <Text>{auth.currentUser.uid}</Text>
+        <Text>{auth.currentUser.email}</Text>
+
+        <View style={styles.userInfoWrapper}>
+          <View style={styles.userInfoItem}>
+          <Text style={styles.userInfoTitle}>5</Text>
+            <Text style={styles.userInfoSubTitle}>Posts</Text>
+          </View>
+          <View style={styles.userInfoItem}>
+            <Text style={styles.userInfoTitle}>10,000</Text>
+            <Text style={styles.userInfoSubTitle}>Likes</Text>
+          </View>
+          <View style={styles.userInfoItem}>
+            <Text style={styles.userInfoTitle}>100</Text>
+            <Text style={styles.userInfoSubTitle}>Dislikes</Text>
+          </View>
+        </View>
+      </View>
+      <FlatList 
+        data={clothes?.filter(data => data?.user === auth.currentUser.uid)}
+        // data={clothes}
+        renderItem = {({ item }) => {
+          return (
+            <ClothItem
+              cloth={item}
+              // onItemPress={itemPressed}
+            />
+          );
+        }}
+        // contentContainerStyle={styles.scrollViewItems}
+      >
+      </FlatList>
+    </SafeAreaView>
+  );
+};
+
+export default MyProfile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  userImg: {
+    height: 80,
+    width: 80,
+    borderRadius: 75,
+  },
+  userName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  aboutUser: {
+    fontSize: 12,
+    fontWeight: '600',
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  userBtnWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    marginBottom: 10,
+  },
+  userBtn: {
+    borderColor: '#2e64e5',
+    borderWidth: 2,
+    borderRadius: 3,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    marginHorizontal: 5,
+  },
+  userBtnTxt: {
+    color: '#2e64e5',
+  },
+  userInfoWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginVertical: 20,
+  },
+  userInfoItem: {
+    justifyContent: 'center',
+  },
+  userInfoTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  userInfoSubTitle: {
+    fontSize: 12,
+    color: '#666',
+    textAlign: 'center',
+  },
+});
