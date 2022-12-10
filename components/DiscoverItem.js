183d04392219bcacde68a21703e9e6ed91616f61
jing520 <78027883+ljin520@users.noreply.github.com>
2022-12-10 01:47:17 -0800

    Update DiscoverItem.js

--- components/DiscoverItem.js
+++ components/DiscoverItem.js
@@ -3,8 +3,7 @@ import {
   View,
   Text,
   StyleSheet,
-  Image,
-  Button,
+  Pressable,
   ImageBackground,
 } from "react-native";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
@@ -12,16 +11,15 @@ import { faMugSaucer } from '@fortawesome/free-solid-svg-icons/faMugSaucer'
 import { useNavigation } from "@react-navigation/native";
 import { addLikesToDB } from '../Firebase/firestore';
 import { addDislikesToDB } from '../Firebase/firestore';
-import { getStorage } from "firebase/storage";
 import { getDownloadURL, ref } from "firebase/storage";
 import { storage } from "../Firebase/firebase-setup";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Icon from "react-native-vector-icons/EvilIcons";
+// import { styles }  from "./styles";
 
 function DiscoverItem({ cloth }) {
     const cur = cloth.imageUri;
     const navigation = useNavigation();
-
     const [imageURL, setImageURL] = useState("");
     useEffect(() => {
         const getImageURL = async () => {
@@ -40,23 +38,28 @@ function DiscoverItem({ cloth }) {
         getImageURL();
       }, []);
 
-      // console.log(imageURL);
+      const username = cloth.user;
       const clothname = cloth.title;
     return (
+      <Pressable onPress={() => {
+        
+        navigation.navigate("ReviewScreen",{cloth});
+      }}
+      android_ripple={{ color: "black", foreground: true }}
+      >
       <View style={styles.card}>
         <ImageBackground
           style={{ flex: 1 }}
           source={{
-            uri: 
-            imageURL
-            // "https://raw.githubusercontent.com/AboutReact/sampleresource/master/crystal_background.jpg",
+            uri: imageURL
           }}
         >
-          <View style={styles.container}>
+          
+          <View style={styles.discovercontainer}>
             <Text style={styles.titleStyle}>{clothname}</Text>
             <View style={styles.centerContentStyle}>
               <FontAwesomeIcon icon={faMugSaucer} />
-              <Text style={styles.TextStyle}>Posted by {cloth.userName == null ? ('anonymous') : cloth.userName}</Text>
+              <Text style={styles.TextStyle}>Posted by {username}</Text>
             </View>
           </View>
   
@@ -97,11 +100,12 @@ function DiscoverItem({ cloth }) {
           </View>
         </ImageBackground>
       </View>
+      </Pressable>
     );
   }
   
   const styles = StyleSheet.create({
-    container: {
+    discovercontainer: {
       flex: 1,
       padding: 10,
     },
@@ -119,45 +123,15 @@ function DiscoverItem({ cloth }) {
     card: {
       flex: 1,
       backgroundColor: "#e9eeef",
-      // margin:15,
-      // marginHorizontal:25,
     },
-    cardHearder: {
-      flex: 1,
-      flexWrap: "wrap",
-      justifyContent: "space-between",
-      marginHorizontal: 20,
-    },
-    headerLeft: {
-      alignItems: "flex-start",
-      width: 100,
-      height: 20,
-    },
-    headerRight: {
-      alignItems: "flex-end",
-      width: 100,
-      height: 20,
-    },
-    userImage: {
-      width: 20,
-      height: 20,
-    },
-  
+
     cardFooter: {
       flex: 1,
     },
     cardFooterLeft: {
       flexDirection: "row-reverse",
     },
-    itemTitle: {
-      fontSize: 16,
-      fontWeight: "bold",
-    },
-    tinyLogo: {
-      flex: 8,
-      width: 250,
-      height: 250,
-    },
+
   });
   
 export default DiscoverItem;
