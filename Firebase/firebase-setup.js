d138874795a2572ed883a1caf1523c325caada75
ClaraLee1 <97991874+ClaraLee1@users.noreply.github.com>
2022-12-12 22:53:31 -0800

    add gitignore .env

--- Firebase/firebase-setup.js
+++ Firebase/firebase-setup.js
@@ -3,17 +3,18 @@ import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId} from "react-native-dotenv";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAt_U-2vf40pQ4EcUnBapA_MkQ5LH4LLt8",
-  authDomain: "amazingclothes-705ac.firebaseapp.com",
-  projectId: "amazingclothes-705ac",
-  storageBucket: "amazingclothes-705ac.appspot.com",
-  messagingSenderId: "591583759383",
-  appId: "1:591583759383:web:169471b50cc39fb2d0e052",
-  // measurementId: "G-965ZKNWTEV"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,       
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 };
-let myApp = initializeApp(firebaseConfig);
+
+const myApp = initializeApp(firebaseConfig);
 
 export const firestore = getFirestore(myApp);
 export const auth = initializeAuth(myApp, {
